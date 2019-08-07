8348b3bb8b32ed31f2a43f40fc19434e2d0816a8
ナッティ <hello@nutty.cc>
2019-08-06 23:33:34 -0700

    Ignore mod tap interrupt

--- config.h
+++ config.h
@@ -28,4 +28,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 
 #define RETRO_TAPPING
-#define TAPPING_TERM 100
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 200
